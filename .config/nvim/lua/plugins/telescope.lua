9fe4eba0c57f8ca9d777653488b92df1add4eb09
Michael Gilhooly <michael.gilhooly@iag.com.au>
2024-08-24 00:22:54 +1000

    More telescope fun

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,130 +1,138 @@
 -- plugins/telescope.lua:
 return { -- Fuzzy Finder (files, lsp, etc)
-  "nvim-telescope/telescope.nvim",
-  event = "VimEnter",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { -- If encountering errors, see telescope-fzf-native README for installation instructions
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      cond = function()
-        return vim.fn.executable("make") == 1
-      end,
+    "nvim-telescope/telescope.nvim",
+    event = "VimEnter",
+    branch = "0.1.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { -- If encountering errors, see telescope-fzf-native README for installation instructions
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+            cond = function()
+                return vim.fn.executable("make") == 1
+            end,
+        },
+        { "nvim-telescope/telescope-file-browser.nvim", enabled = true },
+        { "nvim-telescope/telescope-ui-select.nvim" },
+
+        -- Useful for getting pretty icons, but requires a Nerd Font.
+        { "nvim-tree/nvim-web-devicons",                enabled = vim.g.have_nerd_font },
     },
-    { "nvim-telescope/telescope-file-browser.nvim", enabled = true },
-    { "nvim-telescope/telescope-ui-select.nvim" },
+    config = function()
+        -- [[ Configure Telescope ]]
+        -- See `:help telescope` and `:help telescope.setup()`
+        require("telescope").setup({
+            defaults = {
+                vimgrep_arguments = {
+                    "rg",
+                    "--follow", -- Follow symbolic links
+                    "--hidden", -- Search for hidden files
+                    "--no-heading", -- Don't group matches by each file
+                    "--with-filename", -- Print the file path with the matched lines
+                    "--line-number", -- Show line numbers
+                    "--column", -- Show column numbers
+                    "--smart-case", -- Smart case search
 
-    -- Useful for getting pretty icons, but requires a Nerd Font.
-    { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
-  },
-  config = function()
-    -- [[ Configure Telescope ]]
-    -- See `:help telescope` and `:help telescope.setup()`
-    require("telescope").setup({
-      defaults = {
-        vimgrep_arguments = {
-          "rg",
-          "--follow", -- Follow symbolic links
-          "--hidden", -- Search for hidden files
-          "--no-heading", -- Don't group matches by each file
-          "--with-filename", -- Print the file path with the matched lines
-          "--line-number", -- Show line numbers
-          "--column", -- Show column numbers
-          "--smart-case", -- Smart case search
+                    -- Exclude some patterns from search
+                    "--glob=!**/.git/*",
+                    "--glob=!**/.idea/*",
+                    "--glob=!**/.vscode/*",
+                    "--glob=!**/build/*",
+                    "--glob=!**/dist/*",
+                    "--glob=!**/yarn.lock",
+                    "--glob=!**/package-lock.json",
+                },
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    -- needed to exclude some files & dirs from general search
+                    -- when not included or specified in .gitignore
+                    find_command = {
+                        "rg",
+                        "--files",
+                        "--hidden",
+                        "--glob=!**/.git/*",
+                        "--glob=!**/.obsidian/*",
+                        "--glob=!**/99_archive/*",
+                        "--glob=!**/.idea/*",
+                        "--glob=!**/.vscode/*",
+                        "--glob=!**/build/*",
+                        "--glob=!**/dist/*",
+                        "--glob=!**/yarn.lock",
+                        "--glob=!**/package-lock.json",
+                    },
+                },
+                --   mappings = {
+                --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+                --   },
+            },
+            -- pickers = {}
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown(),
+                },
+                file_browser = {
+                    path = "%:p:h", -- open from within the folder of your current buffer
+                    display_stat = false, -- don't show file stat
+                    grouped = true, -- group initial sorting by directories and then files
+                    hidden = true, -- show hidden files
+                    hide_parent_dir = true, -- hide `../` in the file browser
+                    hijack_netrw = true, -- use telescope file browser when opening directory paths
+                    prompt_path = true, -- show the current relative path from cwd as the prompt prefix
+                    use_fd = true, -- use `fd` instead of plenary, make sure to install `fd`
+                },
+            },
+        })
 
-          -- Exclude some patterns from search
-          "--glob=!**/.git/*",
-          "--glob=!**/.idea/*",
-          "--glob=!**/.vscode/*",
-          "--glob=!**/build/*",
-          "--glob=!**/dist/*",
-          "--glob=!**/yarn.lock",
-          "--glob=!**/package-lock.json",
-        },
-      },
-      pickers = {
-        find_files = {
-          hidden = true,
-          -- needed to exclude some files & dirs from general search
-          -- when not included or specified in .gitignore
-          find_command = {
-            "rg",
-            "--files",
-            "--hidden",
-            "--glob=!**/.git/*",
-            "--glob=!**/.obsidian/*",
-            "--glob=!**/99_archive/*",
-            "--glob=!**/.idea/*",
-            "--glob=!**/.vscode/*",
-            "--glob=!**/build/*",
-            "--glob=!**/dist/*",
-            "--glob=!**/yarn.lock",
-            "--glob=!**/package-lock.json",
-          },
-        },
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-      },
-      -- pickers = {}
-      extensions = {
-        ["ui-select"] = {
-          require("telescope.themes").get_dropdown(),
-        },
-        file_browser = {
-          path = "%:p:h", -- open from within the folder of your current buffer
-          display_stat = false, -- don't show file stat
-          grouped = true, -- group initial sorting by directories and then files
-          hidden = true, -- show hidden files
-          hide_parent_dir = true, -- hide `../` in the file browser
-          hijack_netrw = true, -- use telescope file browser when opening directory paths
-          prompt_path = true, -- show the current relative path from cwd as the prompt prefix
-          use_fd = true, -- use `fd` instead of plenary, make sure to install `fd`
-        },
-      },
-    })
+        -- Enable Telescope extensions if they are installed
+        pcall(require("telescope").load_extension, "fzf")
+        pcall(require("telescope").load_extension, "ui-select")
+        pcall(require("telescope").load_extension, "file_browser")
 
-    -- Enable Telescope extensions if they are installed
-    pcall(require("telescope").load_extension, "fzf")
-    pcall(require("telescope").load_extension, "ui-select")
-    pcall(require("telescope").load_extension, "file_browser")
+        -- See `:help telescope.builtin`
+        local builtin = require("telescope.builtin")
+        vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+        vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+        vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+        vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+        vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+        vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+        vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+        vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+        vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+        vim.keymap.set("n", "<leader>s-", ":Telescope file_browser<CR>", { desc = "[S]earch File[-]Browser" })
+        vim.keymap.set(
+            "n",
+            "<leader>sc",
+            ":Telescope commands<CR>",
+            { desc = "[S]earch [C]ommands", noremap = true, silent = true }
+        )
+        vim.keymap.set("n", "<leader>st", ":TodoTelescope<CR>",
+            { desc = "[F]ind [T]ODOs", noremap = true, silent = true })
 
-    -- See `:help telescope.builtin`
-    local builtin = require("telescope.builtin")
-    vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
-    vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-    vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-    vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
-    vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-    vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-    vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-    vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
-    vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-    vim.keymap.set("n", "<leader>s-", ":Telescope file_browser<CR>", { desc = "[S]earch File[-]Browser" })
+        -- Slightly advanced example of overriding default behavior and theme
+        vim.keymap.set("n", "<leader>/", function()
+            -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+            builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+                winblend = 10,
+                previewer = false,
+            }))
+        end, { desc = "[/] Fuzzily search in current buffer" })
 
-    -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set("n", "<leader>/", function()
-      -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-        winblend = 10,
-        previewer = false,
-      }))
-    end, { desc = "[/] Fuzzily search in current buffer" })
+        -- It's also possible to pass additional configuration options.
+        --  See `:help telescope.builtin.live_grep()` for information about particular keys
+        vim.keymap.set("n", "<leader>s/", function()
+            builtin.live_grep({
+                grep_open_files = true,
+                prompt_title = "Live Grep in Open Files",
+            })
+        end, { desc = "[S]earch [/] in Open Files" })
 
-    -- It's also possible to pass additional configuration options.
-    --  See `:help telescope.builtin.live_grep()` for information about particular keys
-    vim.keymap.set("n", "<leader>s/", function()
-      builtin.live_grep({
-        grep_open_files = true,
-        prompt_title = "Live Grep in Open Files",
-      })
-    end, { desc = "[S]earch [/] in Open Files" })
-
-    -- Shortcut for searching your Neovim configuration files
-    vim.keymap.set("n", "<leader>sn", function()
-      builtin.find_files({ cwd = vim.fn.stdpath("config") })
-    end, { desc = "[S]earch [N]eovim files" })
-  end,
+        -- Shortcut for searching your Neovim configuration files
+        vim.keymap.set("n", "<leader>sn", function()
+            builtin.find_files({ cwd = vim.fn.stdpath("config") })
+        end, { desc = "[S]earch [N]eovim files" })
+    end,
 }
