80bab8b14cb511a7bee770d5762bdf6578a610cc
Michael Gilhooly <michael.gilhooly@iag.com.au>
2024-08-25 21:26:52 +1000

    Update lspconfig to work for golang

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,84 +1,106 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-path", -- source for file system paths
-    "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
-    -- "hrsh7th/cmp-nvim-lsp", -- cmp_nvim_lsp
-    -- "neovim/nvim-lspconfig", -- lspconfig
-    "onsails/lspkind-nvim", -- lspkind (VS pictograms)
-    { "saadparwaiz1/cmp_luasnip", enabled = true }, -- for autocompletion
-    {
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      build = "make install_jsregexp",
-      dependencies = { "rafamadriz/friendly-snippets" }, -- Snippets
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-        -- https://github.com/rafamadriz/friendly-snippets/blob/main/snippets/go.json
-      end,
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-buffer",                       -- source for text in buffer
+        "hrsh7th/cmp-path",                         -- source for file system paths
+        "rafamadriz/friendly-snippets",             -- useful snippets
+        "onsails/lspkind.nvim",                     -- vs-code like pictograms
+        "hrsh7th/cmp-nvim-lsp",                     -- cmp_nvim_lsp
+        "neovim/nvim-lspconfig",                    -- lspconfig
+        "onsails/lspkind-nvim",                     -- lspkind (VS pictograms)
+        { "saadparwaiz1/cmp_luasnip", enabled = true }, -- for autocompletion
+        {
+            "L3MON4D3/LuaSnip",
+            version = "v2.*",
+            build = "make install_jsregexp",
+            dependencies = { "rafamadriz/friendly-snippets" }, -- Snippets
+            config = function()
+                require("luasnip.loaders.from_vscode").lazy_load()
+                -- https://github.com/rafamadriz/friendly-snippets/blob/main/snippets/go.json
+            end,
+        },
     },
-  },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    local lspkind = require("lspkind")
-    local types = require("luasnip.util.types")
+    config = function()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local types = require("luasnip.util.types")
 
-    -- Display virtual text to indicate snippet has more nodes
-    luasnip.config.setup({
-      ext_opts = {
-        [types.choiceNode] = {
-          active = { virt_text = { { "⇥", "GruvboxRed" } } },
-        },
-        [types.insertNode] = {
-          active = { virt_text = { { "⇥", "GruvboxBlue" } } },
-        },
-      },
-    })
+        -- Display virtual text to indicate snippet has more nodes
+        luasnip.config.setup({
+            ext_opts = {
+                [types.choiceNode] = {
+                    active = { virt_text = { { "⇥", "GruvboxRed" } } },
+                },
+                [types.insertNode] = {
+                    active = { virt_text = { { "⇥", "GruvboxBlue" } } },
+                },
+            },
+        })
 
-    -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
-    require("luasnip.loaders.from_vscode").lazy_load()
+        cmp.setup({
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
+            snippet = {
+                expand = function(args)
+                    require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = {
+                ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+                ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+                ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+                ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+                ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+                ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+                ["<C-e>"] = cmp.mapping({
+                    i = cmp.mapping.abort(),
+                    c = cmp.mapping.close(),
+                }),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    elseif require("luasnip").expand_or_jumpable() then
+                        require("luasnip").expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item()
+                    elseif require("luasnip").jumpable(-1) then
+                        require("luasnip").jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+            },
+            sources = cmp.config.sources({
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+            }, {
+                { name = "buffer" },
+            }),
+        })
 
-    cmp.setup({
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      snippet = { -- configure how nvim-cmp interacts with snippet engine
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-        ["<C-e>"] = cmp.mapping.abort(), -- close completion window
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      }),
-      -- sources for autocompletion
-      sources = cmp.config.sources({
-        -- { name = "nvim_lsp" },
-        { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
-        { name = "path" }, -- file system paths
-      }),
-      -- configure lspkind for vs-code like pictograms in completion menu
-      formatting = {
-        format = lspkind.cmp_format({
-          mode = "symbol_text",
-          maxwidth = 50,
-          show_labelDetails = true,
-        }),
-      },
-    })
-    -- local lspconfig = require("lspconfig")
-    -- Python: brew install pyright
-    -- lspconfig["pyright"].setup({})
-  end,
+        -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline("/", {
+            sources = {
+                { name = "buffer" },
+            },
+        })
+
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(":", {
+            sources = cmp.config.sources({
+                { name = "path" },
+            }, {
+                { name = "cmdline" },
+            }),
+        })
+    end,
 }
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -7,143 +7,98 @@ return {
         { "folke/neodev.nvim",                   opts = {} },
     },
     config = function()
-        -- import lspconfig plugin
-        local lspconfig = require("lspconfig")
-
-        -- import mason_lspconfig plugin
-        local mason_lspconfig = require("mason-lspconfig")
-
-        -- import cmp-nvim-lsp plugin
+        local nvim_lsp = require("lspconfig")
+        local keymap = vim.keymap -- for conciseness
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-        local keymap = vim.keymap -- for conciseness
+        -- Common capabilities for LSP servers
+        local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
+        -- List of LSP servers
+        local servers = { "gopls", "pyright", "tsserver" }
+
+        -- Setup each LSP server
+        for _, lsp in ipairs(servers) do
+            nvim_lsp[lsp].setup({
+                capabilities = capabilities,
+                flags = {
+                    debounce_text_changes = 150,
+                },
+            })
+        end
+
+        -- Create autocmd for LspAttach to set keymaps
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(ev)
-                -- Buffer local mappings.
-                -- See `:help vim.lsp.*` for documentation on any of the below functions
                 local opts = { buffer = ev.buf, silent = true }
 
-                -- set keybinds
-                opts.desc = "Show LSP references"
-                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-
-                opts.desc = "Go to declaration"
-                keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
-
-                opts.desc = "Show LSP definitions"
-                keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-                opts.desc = "Show LSP implementations"
-                keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
-
-                opts.desc = "Show LSP type definitions"
-                keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-                opts.desc = "See available code actions"
-                keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-                opts.desc = "Smart rename"
-                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
-
-                opts.desc = "Show buffer diagnostics"
-                keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-                opts.desc = "Show line diagnostics"
-                keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-                opts.desc = "Go to previous diagnostic"
-                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-                opts.desc = "Go to next diagnostic"
-                keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-
-                opts.desc = "Show documentation for what is under cursor"
-                keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-
-                opts.desc = "Restart LSP"
-                keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-            end,
-        })
-
-        -- used to enable autocompletion (assign to every lsp server config)
-        local capabilities = cmp_nvim_lsp.default_capabilities()
-
-        -- Change the Diagnostic symbols in the sign column (gutter)
-        -- (not in youtube nvim video)
-        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
-
-        mason_lspconfig.setup_handlers({
-            -- default handler for installed servers
-            function(server_name)
-                lspconfig[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
-            ["graphql"] = function()
-                -- configure graphql language server
-                lspconfig["graphql"].setup({
-                    capabilities = capabilities,
-                    filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-                })
-            end,
-            ["emmet_ls"] = function()
-                -- configure emmet language server
-                lspconfig["emmet_ls"].setup({
-                    capabilities = capabilities,
-                    filetypes = { "html", "templ", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-                })
-            end,
-            ["templ"] = function()
-                lspconfig["templ"].setup({
-                    capabilities = capabilities,
-                })
-            end,
-            ["gopls"] = function()
-                lspconfig["gopls"].setup({
-                    capabilities = capabilities,
-                    cmd = { "gopls", "serve" },
-                    filetypes = { "go", "gomod", "gotmpl", "gowork" },
-                    root_dir = lspconfig.util.root_pattern("go.mod", "go.work", ".git"),
-                    settings = {
-                        gopls = {
-                            completeUnimported = true,
-                            usePlaceholders = true,
-                            staticcheck = true,
-                            analyses = {
-                                unusedparams = true,
-                            },
-                        },
-                    },
-                })
-            end,
-            ["html"] = function()
-                lspconfig["html"].setup({
-                    capabilities = capabilities,
-                    filetypes = { "html", "templ" },
-                })
-            end,
-            ["lua_ls"] = function()
-                -- configure lua server (with special settings)
-                lspconfig["lua_ls"].setup({
-                    capabilities = capabilities,
-                    settings = {
-                        Lua = {
-                            -- make the language server recognize "vim" global
-                            diagnostics = {
-                                globals = { "vim" },
-                            },
-                            completion = {
-                                callSnippet = "Replace",
-                            },
-                        },
-                    },
-                })
+                -- Set keybinds
+                keymap.set(
+                    "n",
+                    "gR",
+                    "<cmd>Telescope lsp_references<CR>",
+                    { desc = "Show LSP references", buffer = ev.buf, silent = true }
+                )
+                keymap.set("n", "gD", vim.lsp.buf.declaration,
+                    { desc = "Go to declaration", buffer = ev.buf, silent = true })
+                keymap.set(
+                    "n",
+                    "gd",
+                    "<cmd>Telescope lsp_definitions<CR>",
+                    { desc = "Show LSP definitions", buffer = ev.buf, silent = true }
+                )
+                keymap.set(
+                    "n",
+                    "gi",
+                    "<cmd>Telescope lsp_implementations<CR>",
+                    { desc = "Show LSP implementations", buffer = ev.buf, silent = true }
+                )
+                keymap.set(
+                    "n",
+                    "gt",
+                    "<cmd>Telescope lsp_type_definitions<CR>",
+                    { desc = "Show LSP type definitions", buffer = ev.buf, silent = true }
+                )
+                keymap.set(
+                    { "n", "v" },
+                    "<leader>ca",
+                    vim.lsp.buf.code_action,
+                    { desc = "See available code actions", buffer = ev.buf, silent = true }
+                )
+                keymap.set("n", "<leader>rn", vim.lsp.buf.rename,
+                    { desc = "Smart rename", buffer = ev.buf, silent = true })
+                keymap.set(
+                    "n",
+                    "<leader>D",
+                    "<cmd>Telescope diagnostics bufnr=0<CR>",
+                    { desc = "Show buffer diagnostics", buffer = ev.buf, silent = true }
+                )
+                keymap.set(
+                    "n",
+                    "<leader>d",
+                    vim.diagnostic.open_float,
+                    { desc = "Show line diagnostics", buffer = ev.buf, silent = true }
+                )
+                keymap.set(
+                    "n",
+                    "[d",
+                    vim.diagnostic.goto_prev,
+                    { desc = "Go to previous diagnostic", buffer = ev.buf, silent = true }
+                )
+                keymap.set(
+                    "n",
+                    "]d",
+                    vim.diagnostic.goto_next,
+                    { desc = "Go to next diagnostic", buffer = ev.buf, silent = true }
+                )
+                keymap.set(
+                    "n",
+                    "K",
+                    vim.lsp.buf.hover,
+                    { desc = "Show documentation for what is under cursor", buffer = ev.buf, silent = true }
+                )
+                keymap.set("n", "<leader>rs", ":LspRestart<CR>", { desc = "Restart LSP", buffer = ev.buf, silent = true })
             end,
         })
     end,
