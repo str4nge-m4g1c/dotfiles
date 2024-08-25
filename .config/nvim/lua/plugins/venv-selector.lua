30ad026bafde60db48a5d1f25b2c035858e8a360
Michael Gilhooly <michael.gilhooly@iag.com.au>
2024-08-26 09:21:39 +1000

    Configuration and setup of venv-selector

--- .config/nvim/lua/plugins/venv-selector.lua
+++ .config/nvim/lua/plugins/venv-selector.lua
@@ -2,9 +2,7 @@ return {
     "linux-cultist/venv-selector.nvim",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
     opts = {
-        -- Your options go here
-        -- name = "venv",
-        -- auto_refresh = false
+        stay_on_this_version = true,
     },
     event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
     keys = {
