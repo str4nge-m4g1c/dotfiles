6218b135628339c92376fc973b03e60b33db0cdc
Michael Gilhooly <michael.gilhooly@iag.com.au>
2024-09-09 11:51:36 +1000

    Added markdown preview and removed render-markdown

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
+  keys = {
+    { "<leader>pm", "<cmd>MarkdownPreviewToggle<cr>", mode = "n", desc = "Toggle previewer" },
+  },
+}
