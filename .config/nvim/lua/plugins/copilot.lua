da6ae09c84b732f2c6f0249e3aeb1451d5ebf757
Michael Gilhooly <michael.gilhooly@iag.com.au>
2024-09-11 21:10:04 +1000

    Removed neogit, added keybindings for gitsigns,removed commented code from copilot

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,46 +1,4 @@
 return {
-    'github/copilot.vim', -- Install CoPilot that allows code suggestions/completions
-    -- {
-    --   'CopilotC-Nvim/CopilotChat.nvim',
-    --   branch = 'canary',
-    --   dependencies = {
-    --     { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
-    --     { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
-    --   },
-    --   opts = {
-    --     debug = false, -- Enable debug logging
-    --     proxy = nil, -- [protocol://]host[:port] Use this proxy
-    --     allow_insecure = false, -- Allow insecure server connections
-    --   },
-    --   keys = {
-    --     {
-    --       desc = 'CopilotChat - Quick chat',
-    --       '<leader>ccq',
-    --       function()
-    --         local input = vim.fn.input 'Quick Chat: '
-    --         if input ~= '' then
-    --           require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
-    --         end
-    --       end,
-    --     },
-    --     -- Show help actions with telescope
-    --     {
-    --       '<leader>cch',
-    --       function()
-    --         local actions = require 'CopilotChat.actions'
-    --         require('CopilotChat.integrations.telescope').pick(actions.help_actions())
-    --       end,
-    --       desc = 'CopilotChat - Help actions',
-    --     },
-    --     -- Show prompts actions with telescope
-    --     {
-    --       '<leader>ccp',
-    --       function()
-    --         local actions = require 'CopilotChat.actions'
-    --         require('CopilotChat.integrations.telescope').pick(actions.prompt_actions())
-    --       end,
-    --       desc = 'CopilotChat - Prompt actions',
-    --     },
-    --   },
-    -- },
+  "github/copilot.vim", -- Install CoPilot that allows code suggestions/completions
 }
+
--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,5 +2,8 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     require("gitsigns").setup()
+    -- Keybindings
+    vim.keymap.set("n", "<leader>tgp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>tgb", ":Gitsigns toggle_current_line_blame<CR>", {})
   end,
 }
