70f7b5d92a9f7c0cabaf5e3d6a325fe804589dfb
Michael Gilhooly <michael.gilhooly@iag.com.au>
2024-12-20 14:41:06 +1100

    Removed terminal shortcuts

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -114,13 +114,13 @@ return {
       end,
       desc = "Dismiss All Notifications",
     },
-    {
-      "<c-/>",
-      function()
-        Snacks.terminal()
-      end,
-      desc = "Toggle Terminal",
-    },
+    -- {
+    --   "<c-/>",
+    --   function()
+    --     Snacks.terminal()
+    --   end,
+    --   desc = "Toggle Terminal",
+    -- },
     -- {
     --   "<c-_>",
     --   function()
