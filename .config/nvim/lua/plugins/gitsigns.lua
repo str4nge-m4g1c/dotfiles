b5fbe05a783718730457ee16954258d324107413
Michael Gilhooly <michael.gilhooly@iag.com.au>
2024-12-20 15:17:17 +1100

    Removed keybindings for gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -2,8 +2,5 @@ return {
   "lewis6991/gitsigns.nvim",
   config = function()
     require("gitsigns").setup()
-    -- Keybindings
-    vim.keymap.set("n", "<leader>tgp", ":Gitsigns preview_hunk<CR>", {})
-    vim.keymap.set("n", "<leader>tgb", ":Gitsigns toggle_current_line_blame<CR>", {})
   end,
 }
