155054442521b637663164e557e9cb950f6d7522
Michael Gilhooly <michael.gilhooly@iag.com.au>
2025-01-07 11:30:31 +1100

    lsp is backup and running

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -9,5 +9,14 @@ return {
 
     -- Disable default tab mapping
     vim.g.copilot_no_tab_map = true
+
+    -- Keymap to toggle Copilot on and off
+    vim.api.nvim_set_keymap('n', '<leader>cp', ':Copilot<CR>', { noremap = true, silent = true })
+
+    -- Keymap to turn off Copilot autosuggestions
+    vim.api.nvim_set_keymap('n', '<leader>coff', ':Copilot disable<CR>', { noremap = true, silent = true })
+
+    -- Keymap to turn on Copilot autosuggestions
+    vim.api.nvim_set_keymap('n', '<leader>con', ':Copilot enable<CR>', { noremap = true, silent = true })
   end
 }
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,62 +1,186 @@
 return {
-    -- LSP and completion plugins
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/nvim-cmp" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-cmdline" },
-    { "hrsh7th/cmp-vsnip" },
-    { "hrsh7th/vim-vsnip" },
-
-    -- Setup nvim-cmp.
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      { "antosha417/nvim-lsp-file-operations", config = true },
+      { "folke/neodev.nvim", opts = {} },
+    },
     config = function()
-        local cmp = require'cmp'
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                end,
-            },
-            mapping = {
-                ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-                ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-                ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-                ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-                ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-                ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-                ['<C-e>'] = cmp.mapping({
-                    i = cmp.mapping.abort(),
-                    c = cmp.mapping.close(),
-                }),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
-            },
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'vsnip' },
-            }, {
-                { name = 'buffer' },
-            })
+      local nvim_lsp = require("lspconfig")
+      local keymap = vim.keymap -- for conciseness
+      local cmp_nvim_lsp = require("cmp_nvim_lsp")
+  
+      -- Common capabilities for LSP servers
+      local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  
+      -- List of LSP servers
+      local servers = { "gopls", "pyright", "ts_ls" }
+  
+      -- Setup each LSP server
+      for _, lsp in ipairs(servers) do
+        nvim_lsp[lsp].setup({
+          capabilities = capabilities,
+          flags = {
+            debounce_text_changes = 150,
+          },
         })
-
-        -- Setup lspconfig.
-        local lspconfig = require'lspconfig'
-
-        -- Enable the following language servers
-        local servers = { 'pyright', 'tsserver', 'gopls', 'rust_analyzer' }
-        for _, lsp in ipairs(servers) do
-            lspconfig[lsp].setup {
-                capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-            }
-        end
-
-        -- Additional configuration for specific language servers
-        lspconfig.pyright.setup{}
-        lspconfig.tsserver.setup{}
-        lspconfig.gopls.setup{}
-        lspconfig.rust_analyzer.setup{}
-    end
-}
+      end
+  
+      -- Specific settings for gopls
+      nvim_lsp.gopls.setup({
+        capabilities = capabilities,
+        flags = {
+          debounce_text_changes = 150,
+        },
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+          },
+        },
+      })
+  
+      -- Specific settings for pyright
+      nvim_lsp.pyright.setup({
+        capabilities = capabilities,
+        filetypes = { "python" },
+        flags = {
+          debounce_text_changes = 150,
+        },
+        settings = {
+          python = {
+            analysis = {
+              typeCheckingMode = "basic",
+              diagnosticMode = "workspace",
+              autoImportCompletions = true,
+              useLibraryCodeForTypes = true,
+            },
+          },
+        },
+      })
+  
+      -- Create autocmd for LspAttach to set keymaps
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf, silent = true }
+  
+          -- Set keybinds
+          keymap.set(
+            "n",
+            "gr",
+            "<cmd>Telescope lsp_references<CR>",
+            { desc = "Show LSP references", buffer = ev.buf, silent = true }
+          )
+          keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration", buffer = ev.buf, silent = true })
+          keymap.set(
+            "n",
+            "gs",
+            "<cmd>Telescope lsp_document_symbols<CR>",
+            { desc = "Show LSP document symbols", buffer = ev.buf, silent = true }
+          )
+          keymap.set(
+            "n",
+            "gd",
+            "<cmd>Telescope lsp_definitions<CR>",
+            { desc = "Show LSP definitions", buffer = ev.buf, silent = true }
+          )
+          keymap.set(
+            "n",
+            "gi",
+            "<cmd>Telescope lsp_implementations<CR>",
+            { desc = "Show LSP implementations", buffer = ev.buf, silent = true }
+          )
+          keymap.set(
+            "n",
+            "gt",
+            "<cmd>Telescope lsp_type_definitions<CR>",
+            { desc = "Show LSP type definitions", buffer = ev.buf, silent = true }
+          )
+          keymap.set(
+            "n",
+            "<leader>li",
+            "<cmd>Telescope lsp_incoming_calls<CR>",
+            { desc = "Show LSP incoming calls", buffer = ev.buf, silent = true }
+          )
+          keymap.set(
+            "n",
+            "<leader>lo",
+            "<cmd>Telescope lsp_outgoing_calls<CR>",
+            { desc = "Show LSP outgoing calls", buffer = ev.buf, silent = true }
+          )
+          keymap.set(
+            { "n", "v" },
+            "<leader>la",
+            vim.lsp.buf.code_action,
+            { desc = "See available code actions", buffer = ev.buf, silent = true }
+          )
+          keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { desc = "Smart rename", buffer = ev.buf, silent = true })
+          keymap.set(
+            "n",
+            "<leader>lD",
+            "<cmd>Telescope diagnostics bufnr=0<CR>",
+            { desc = "Show buffer diagnostics", buffer = ev.buf, silent = true }
+          )
+          keymap.set(
+            "n",
+            "<leader>ld",
+            vim.diagnostic.open_float,
+            { desc = "Show line diagnostics", buffer = ev.buf, silent = true }
+          )
+          keymap.set(
+            "n",
+            "[d",
+            vim.diagnostic.goto_prev,
+            { desc = "Go to previous diagnostic", buffer = ev.buf, silent = true }
+          )
+          keymap.set(
+            "n",
+            "]d",
+            vim.diagnostic.goto_next,
+            { desc = "Go to next diagnostic", buffer = ev.buf, silent = true }
+          )
+          keymap.set(
+            "n",
+            "K",
+            vim.lsp.buf.hover,
+            { desc = "Show documentation for what is under cursor", buffer = ev.buf, silent = true }
+          )
+          keymap.set("n", "<leader>ls", ":LspRestart<CR>", { desc = "Restart LSP", buffer = ev.buf, silent = true })
+  
+          -- Go specific keymaps
+          if vim.bo.filetype == "go" then
+            -- Additional keymap for organizing imports in Go
+            keymap.set(
+              "n",
+              "<C-i>",
+              "<cmd>lua vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })<CR>",
+              { desc = "Organize Imports", buffer = ev.buf, silent = true }
+            )
+            -- Keymaps for switching between Go source and test files
+            keymap.set(
+              "n",
+              "<leader>tt",
+              ":lua require('config.go_utils').switch_to_test()<CR>",
+              { desc = "Switch to test file", buffer = ev.buf, silent = true }
+            )
+            keymap.set(
+              "n",
+              "<leader>tv",
+              ":lua require('config.go_utils').switch_to_test_vsplit()<CR>",
+              { desc = "Switch to test file in vertical split", buffer = ev.buf, silent = true }
+            )
+            keymap.set(
+              "n",
+              "<leader>ts",
+              ":lua require('config.go_utils').switch_to_source()<CR>",
+              { desc = "Switch to source file", buffer = ev.buf, silent = true }
+            )
+          end
+        end,
+      })
+    end,
+  }
