e1fbf705e55d4062a8c71bc000bcc37e08ae1fb8
Michael Gilhooly <michael.gilhooly@iag.com.au>
2025-09-21 20:37:38 +1000

    Updated yazi config

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,5 @@
 function Linemode:size_and_mtime()
-	local time = math.floor(self._file.cha.modified or 0)
+	local time = math.floor(self._file.cha.mtime or 0)
 	if time == 0 then
 		time = ""
 	elseif os.date("%Y", time) == os.date("%Y") then
@@ -9,5 +9,5 @@ function Linemode:size_and_mtime()
 	end
 
 	local size = self._file:size()
-	return ui.Line(string.format("%s %s", size and ya.readable_size(size) or "-", time))
+	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
 end
